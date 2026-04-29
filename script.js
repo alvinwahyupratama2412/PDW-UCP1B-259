0ff90c061f985e39df737c70e0629b08f6f81530
Alvin Wahyu P <alvinwahyu1224@gmail.com>
2026-04-29 22:33:44 +0700

    Menambahkan fitur form input anggota

--- script.js
+++ script.js
@@ -3,13 +3,30 @@ let data = [
     { nama: "Siti", email: "siti@gmail.com", minat: "Data" }
 ];
 
-if (document.getElementById("tableData")) {
+if (document.getElementById("tableData")) 
+{
     let table = document.getElementById("tableData");
 
-    data.forEach(d => {
+    data.forEach(d => 
+    {
         let row = table.insertRow();
         row.insertCell(0).innerText = d.nama;
         row.insertCell(1).innerText = d.email;
         row.insertCell(2).innerText = d.minat;
+    });
+}
+
+let form = document.getElementById("formData");
+
+if (form) 
+{
+    form.addEventListener("submit", function(e) 
+    {
+        e.preventDefault();
+
+        let nama = document.getElementById("nama").value;
+        let email = document.getElementById("email").value;
+        let minat = document.getElementById("minat").value;
+
     });
 }
