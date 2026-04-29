61f2f79236b4c7f8d6aba76e1cca71329c03dd23
Alvin Wahyu P <alvinwahyu1224@gmail.com>
2026-04-29 22:31:59 +0700

    Menampilkan data anggota ke tabel

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 let data = [
     { nama: "Budi", email: "budi@gmail.com", minat: "Programming" },
-    { nama: "Siti", email: "siti@gmail.com", minat: "Design" }
+    { nama: "Siti", email: "siti@gmail.com", minat: "Data" }
 ];
+
+if (document.getElementById("tableData")) {
+    let table = document.getElementById("tableData");
+
+    data.forEach(d => {
+        let row = table.insertRow();
+        row.insertCell(0).innerText = d.nama;
+        row.insertCell(1).innerText = d.email;
+        row.insertCell(2).innerText = d.minat;
+    });
+}
